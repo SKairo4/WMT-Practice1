b4bccf5a3a1e86b1e5533aa77a885ef87f1ba461
SadeshRaj <sadeshrajapakshe@gmail.com>
2026-04-26 23:25:55 +0530

    updated front url

--- frontend3-main/src/api/itemApi.js
+++ frontend3-main/src/api/itemApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:4000/api",
+  baseURL: import.meta.env.VITE_API_URL || "https://wmt-practice1.onrender.com/api",
 });
 
 export const getItems = () => API.get("/items");
